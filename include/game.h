0ab5be8fad3da7eac8a06ae8127a80e97e85bf58
dudu233 <1147793932@qq.com>
2024-06-11 20:37:18 +0800

    创建游戏类 game.cpp game.h

---
+++ include/game.h
@@ -0,0 +1,15 @@
+//
+// Created by dudu233 on 2024/6/11.
+//
+
+#ifndef WHACK_A_MOLE_GAME_H
+#define WHACK_A_MOLE_GAME_H
+
+class game {
+private:
+    unsigned short level;     // 当前关卡等级
+    unsigned short difficulty;// 游戏难度
+};
+
+
+#endif//WHACK_A_MOLE_GAME_H
---
+++ src/game.cpp
@@ -0,0 +1,6 @@
+//
+// Created by dudu233 on 2024/6/11.
+//
+
+#include "game.h"
+#include "debug.h"
