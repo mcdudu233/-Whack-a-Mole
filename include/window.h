683b24af03861d9b2d058c1852d3eeb38771e889
NINI233 <3116278526@qq.com>
2024-06-11 21:30:06 +0800

    nini测试推送

--- include/window.h
+++ include/window.h
@@ -10,7 +10,7 @@
 // 默认分辨率大小 800*600
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
-
+#define BUTTON_MAIN_TEXTCOLOR RGB(255, 255, 255)
 #define BUTTON_MAIN_COLOR RGB(0xC3, 0x4A, 0x36)
 
 void init_graph(int width, int height);
--- src/window.cpp
+++ src/window.cpp
@@ -65,9 +65,9 @@ std::thread init_main_graph() {
     loadimage(&img, getPic("main"), 800, 600);
     putimage(0, 0, &img);
     // 绘制按钮
-    drawButton(280, 380, 220, 80, BUTTON_MAIN_COLOR, "开始游戏", 40, RGB(255, 255, 255));
-    drawButton(20, 530, 100, 50, BUTTON_MAIN_COLOR, "设置", 30, RGB(255, 255, 255));
-    drawButton(640, 530, 140, 50, BUTTON_MAIN_COLOR, "退出游戏", 30, RGB(255, 255, 255));
+    drawButton(280, 380, 220, 80, BUTTON_MAIN_COLOR, "开始游戏", 40, BUTTON_MAIN_TEXTCOLOR);
+    drawButton(20, 530, 100, 50, BUTTON_MAIN_COLOR, "设置", 30, BUTTON_MAIN_TEXTCOLOR);
+    drawButton(640, 530, 140, 50, BUTTON_MAIN_COLOR, "退出游戏", 30, BUTTON_MAIN_TEXTCOLOR);
     // 返回按钮点击监听线程
     return std::thread(main_listener);
 }
