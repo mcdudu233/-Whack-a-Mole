b1ef1ad30d6e7f7b262b65cd1f24574ae646c761
dudu233 <1147793932@qq.com>
2024-06-12 19:35:03 +0800

    更新 main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,17 @@
 #include "conio.h"
 #include "debug.h"
+#include "game.h"
 #include "graphics.h"
-#include "mole.h"
 #include "window.h"
 
 int main() {
     debug("starting program.");
     srand((unsigned) time(NULL));// 初始化随机种子
 
-    // 我叫倪宝成
+    // 初始化界面
     init_graph(800, 600);
     init_main_graph().join();
-    //    mole m1, m2;
-    //    m1.show();
-    //    m2.show();
+
     _getch();    // 按任意键继续
     closegraph();// 关闭绘图窗口
     return 0;
