6d4bd3aa361a1accfd24dfb0c53d334a87b74998
dudu233 <1147793932@qq.com>
2024-06-12 18:59:29 +0800

    更新 game.h 完善游戏类的结构

--- include/game.h
+++ include/game.h
@@ -5,10 +5,40 @@
 #ifndef WHACK_A_MOLE_GAME_H
 #define WHACK_A_MOLE_GAME_H
 
+#include "mole.h"
+#include "vector"
+
+// 游戏难度
+typedef enum { EASY,
+               NORMAL,
+               HARD } Difficulty;
+
+// 地鼠洞的结构体
+typedef struct {
+    unsigned int x;
+    unsigned int y;
+    unsigned int width;
+    unsigned int height;
+    mole mole;
+} Hole;
+
 class game {
 private:
-    unsigned short level;     // 当前关卡等级
-    unsigned short difficulty;// 游戏难度
+    unsigned short level;                // 当前关卡等级
+    Difficulty difficulty;               // 游戏难度
+    unsigned int score;                  // 当前得分
+    std::vector<std::vector<Hole>> holes;// 当前关卡地鼠洞的二维数组
+private:
+    void initializeHoles();// 初始化地鼠洞
+    void spawnMoles();     // 随机生成地鼠
+public:
+    game(unsigned short level, Difficulty diff); // 构造函数
+    ~game();                                     // 析构函数
+    void startNewLevel();                        // 开始新关卡
+    void hitMole(unsigned int x, unsigned int y);// 击打地鼠
+    unsigned int getScore() const;               // 获取当前得分
+    unsigned short getLevel() const;             // 获取当前关卡等级
+    void increaseLevel();                        // 增加关卡等级
 };
 
 
